77da79ffa2295a9f6011509f03e5c38d634acd5e
ssk090 <shivanandasai.38@gmail.com>
2021-11-13 21:44:29 +0530

    code uploaded

---
+++ script.js
@@ -0,0 +1,7 @@
+fetch('https://v2.jokeapi.dev/joke/Programming?type=single')
+.then(data => data.json())
+.then(jokeData =>{
+    const jokeText = jokeData.joke;
+    const jokeElement = document.getElementById('jokeElement');
+    jokeElement.innerHTML = jokeText;
+})
